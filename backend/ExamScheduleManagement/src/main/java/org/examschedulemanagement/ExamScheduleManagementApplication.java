f27dc02c1c6210213d9c3eff1b5d0b126c2bbd7a
younik2304 <youssefanik@gmail.com>
2024-05-02 21:01:11 +0100

    first setup of front and back end

---
+++ backend/ExamScheduleManagement/src/main/java/org/examschedulemanagement/ExamScheduleManagementApplication.java
@@ -0,0 +1,13 @@
+package org.examschedulemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamScheduleManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamScheduleManagementApplication.class, args);
+    }
+
+}
---
+++ backend/ExamScheduleManagement/src/test/java/org/examschedulemanagement/ExamScheduleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.examschedulemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamScheduleManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
